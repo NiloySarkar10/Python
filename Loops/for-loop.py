46b42728dbf2ec22444a95093e48d6600ebd0973
NiloySarkar10 <nlysrkr10@gmail.com>
2022-12-12 13:24:18 +0530

    Loops learning
    Learnt creating basic for loop and while loop

---
+++ Loops/for-loop.py
@@ -0,0 +1,6 @@
+# Learning for loops in python
+
+dummy_list = [1,2,3,4,5,6,7,8,9]
+
+for value in dummy_list:
+    print(value)
---
+++ Loops/while-loop.py
@@ -0,0 +1,8 @@
+# Learning while loop in Python
+
+counter = 1
+
+while counter <= 5:
+    print('Counter: ' + str(counter))
+    counter = counter + 1
+
